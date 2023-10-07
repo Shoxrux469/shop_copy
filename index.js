c151f14a31a30891a5547bf504e42fd1bef6c3a6
Шохрух <138532283+Shoxrux469@users.noreply.github.com>
2023-10-07 14:34:06 +0500

    third com

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ let cont = doc.querySelector(".container");
 let form = doc.forms.form;
 let show_5 = doc.querySelector(".show_5");
 let show_all = doc.querySelector(".show_all");
+let box_num = doc.querySelector(".box_num");
+let content = doc.querySelector(".content");
+let b_window = doc.querySelector(".b_window");
+let checkbox = doc.querySelector("#checkbox");
+let toggle = doc.querySelector(".toggle");
 // console.log(arr.slice(0, 5));
 // show_5.onclick = (e) => {
 //   e.preventDefault();
@@ -29,7 +34,8 @@ function create(arr) {
     let box1img = doc.createElement("img");
     let box2img = doc.createElement("img");
     let box3img = doc.createElement("img");
-    
+    let b_window_p = doc.createElement("p");
+
     mainDiv.classList.add("mainDiv");
     mainImg.classList.add("mainImg");
     middleDiv.classList.add("middleDiv");
@@ -46,17 +52,16 @@ function create(arr) {
     box1img.classList.add("boxImg");
     box2img.classList.add("boxImg");
     box3img.classList.add("boxImg");
-    
+
     mainImg.setAttribute("src", item.image);
     box1img.setAttribute("src", "./icons/price.svg");
     box2img.setAttribute("src", "./icons/star.svg");
     box3img.setAttribute("src", "./icons/box.svg");
     
     h1.innerHTML = `${item.category} (${item.rating.count})`;
-    p.innerHTML =
     item.description.length > 100
-    ? item.description.slice(0, 90) + " <b>...read</b>"
-    : item.description;
+        ? item.description.slice(0, 90) + " <b>...read</b>"
+        : item.description;
     box1p.innerHTML = item.price;
     box2p.innerHTML = item.rating.rate;
     box3p.innerHTML = item.rating.count;
@@ -69,15 +74,33 @@ function create(arr) {
     bottomDivBox1.append(box1img, box1p);
     bottomDivBox2.append(box2img, box2p);
     bottomDivBox3.append(box3img, box3p);
+    content.append(b_window_p)
+    
+    btn.onclick = (e) => {
+      e.preventDefault();
+      console.log("click");
+      box_num.innerHTML++;
+      b_window_p.innerHTML = item.category
+      b_window_p.push(item.category)
+    };
     show_5.onclick = (e) => {
       cont.innerHTML = "";
       a = arr.slice(0, 5);
       e.preventDefault();
       create(a);
     };
+    toggle.onclick = () => {
+      // if(checkbox.status !== 'checked'){
+      //   b_window.classList.remove('w_active')
+      //   alert('Not Hi');
+      // } else {
+        //   b_window.classList.add('w_active')
+        //   alert('Hi');
+        // }
+          b_window.classList.toggle('w_active')
+      }
   }
 }
-
 show_all.onclick = (e) => {
   cont.innerHTML = "";
   e.preventDefault();
